10c1e1fc69d7ff4a62274059652842ee7c7b2161
Ivan Suchilin <104725388+IvanSuchilin@users.noreply.github.com>
2023-12-12 18:24:51 +0300

    added configuration classes

---
+++ src/main/java/com/suchilin/WebSocket/security/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.suchilin.WebSocket.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@EnableWebSecurity
+public class SecurityConfig {
+    @Autowired
+    public void autoConfigure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user1")
+                .password("{noop}password")
+                .roles("GUEST")
+                .and()
+                .withUser("user2")
+                .password("{noop}password")
+                .roles("GUEST")
+                .and()
+                .withUser("user3")
+                .password("{noop}password")
+                .roles("GUEST");
+    }
+}
