17a530642a1b4196ffd7211e2083e15618862f0c
Ivan Suchilin <104725388+IvanSuchilin@users.noreply.github.com>
2023-12-13 18:07:15 +0300

    fixed

--- src/main/java/com/suchilin/WebSocket/security/SecurityConfig.java
+++ src/main/java/com/suchilin/WebSocket/security/SecurityConfig.java
@@ -1,10 +1,11 @@
 package com.suchilin.WebSocket.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.password.PasswordEncoder;
-
+@Configuration
 @EnableWebSecurity
 public class SecurityConfig {
     @Autowired
